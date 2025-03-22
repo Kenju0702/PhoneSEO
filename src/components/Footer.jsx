2e3f589e47a50bd4d166df8c3afd0f9732015946
QTruong1412 <tn7253591@gmail.com>
2025-03-22 11:44:27 +0700

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,40 +1,250 @@
 import React from 'react'
 
 export const FooterPage = () => {
-  return (
-<footer className="bg-gray-300  shadow  ">
-    <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div className="sm:flex sm:items-center sm:justify-between">
-            <a href="#" className="flex items-center mb-4 sm:mb-0">
-                <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
-                <span className="self-center text-2xl font-semibold whitespace-nowrap">Flowbite</span>
-            </a>
-            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-black sm:mb-0">
-                <li>
-                    <a href="#" className="mr-4 hover:underline md:mr-6">About</a>
-                </li>
-                <li>
-                    <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-                </li>
-                <li>
-                    <a href="#" className="mr-4 hover:underline md:mr-6">Licensing</a>
-                </li>
-                <li>
-                    <a href="#" className="hover:underline">Contact</a>
-                </li>
-                <li>
-                    <a href="#" className="hover:underline">Cointcard</a>
-                </li>
-                <li>
-                    <a href="#" className="hover:underline">mood</a>
-                </li>
-            </ul>
-        </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
-    </div>
-</footer>
+    return (
+        <footer className="bg-gray-950  shadow  ">
+            <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+                <div className="sm:flex sm:items-center sm:justify-between">
+                    <a href="#" className="flex items-center mb-4 sm:mb-0">
+                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Flowbite</span>
+                    </a>
+
+                </div>
+                <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+
+                <div className="flex flex-row text-white content-around w-full text-base">
+                    <div className="basis-sm">
+                        <ul className="grid grid-cols-1 gap-2 basic-sm">
+                            <li className="basis-sm">KẾT NỐI VỚI CHÚNG TÔI</li>
+                            <li className="basis-sm">
+                                <ul className="flex">
+                                    <li className="flex flex-nowrap ml-1">
+                                        <a href='#'>
+                                            <img src='https://cdn2.fptshop.com.vn/svg/facebook_icon_8543190720.svg?w=32&q=100 1x, https://cdn2.fptshop.com.vn/svg/facebook_icon_8543190720.svg?w=64&q=100 2x' width={30} height={30}></img>
+                                        </a>
+                                    </li>
+                                    <li className="flex ml-1">
+                                        <a href='#'>
+                                            <img src='https://cdn2.fptshop.com.vn/svg/zalo_icon_8cbef61812.svg?w=32&q=100 1x, https://cdn2.fptshop.com.vn/svg/zalo_icon_8cbef61812.svg?w=64&q=100 2x' width={30} height={30}></img>
+
+                                        </a>
+                                    </li>
+                                    <li className="flex ml-1">
+                                        <a href='#'>
+                                            <img src='https://cdn2.fptshop.com.vn/svg/youtube_icon_b492d61ba5.svg?w=32&q=100 1x, https://cdn2.fptshop.com.vn/svg/youtube_icon_b492d61ba5.svg?w=64&q=100 2x' width={30} height={30}></img>
+
+                                        </a>
+                                    </li>
+                                    <li className="flex ml-1">
+                                        <a href='#'>
+                                            <img src='https://cdn2.fptshop.com.vn/svg/tiktok_icon_faabbeeb61.svg?w=32&q=100 1x, https://cdn2.fptshop.com.vn/svg/tiktok_icon_faabbeeb61.svg?w=64&q=100 2x' width={30} height={30}></img>
+                                        </a></li>
+                                </ul>
+                            </li>
+                            <li className="basis-xm">
+                                <a href='#' className='hover:underline'>Tổng đài miễn phí</a></li>
+                            <li className="basis-xm">
+                                <a href='#' className='hover:underline'>Tư vấn mua hàng(Miễn phí)</a></li>
+                            <li className="basis-xm">
+                                <a href='#' className='hover:underline'>Góp ý khiếu nại</a></li>
+                        </ul>
+                    </div>
+                    <div className="basis-sm">
+                        <ul className="grid grid-cols-1 gap-2 basic-sm">
+                            <li className="basis-sm">VỀ CHÚNG TÔI</li>
+                            <li className="basis-sm" href='#'>
+                                <a href='#' className='hover:underline'>Giới thiệu về công ty</a>
+                            </li>
+                            <li className="basis-sm" href='#'>
+                                <a href='#' className='hover:underline'>Quy chế hoạt động</a></li>
+                            <li className="basis-sm" href='#'>
+                                <a href='#' className='hover:underline'>Dự án doanh nghiệp</a></li>
+                            <li className="basis-sm" href='#'>
+                                <a href='#' className='hover:underline'>Tin tức khuyến mãi</a></li>
+                            <li className="basis-sm" href='#'>
+                                <a href='#' className='hover:underline'>Giới thiệu máy đổi trả</a></li>
+                            <li className="basis-sm" href='#'>
+                                <a href='#' className='hover:underline'>Hướng dẫn mua hàng & thanh toán online</a></li>
+                            <li className="basis-sm" href='#'>
+                                <a href='#' className='hover:underline'>Đại lý uỷ quyền và TTBH uỷ quyền của Apple</a></li>
+                            <li className="basis-sm" href='#'>
+                                <a href='#' className='hover:underline'>Tra cứu hoá đơn điện tử</a></li>
+                            <li className="basis-sm" href='#'>
+                                <a href='#' className='hover:underline'>Tra cứu bảo hành</a></li>
+                            <li className="basis-sm" href='#'>
+                                <a href='#' className='hover:underline'>Câu hỏi thường gặp</a></li>
+                        </ul>
+                    </div>
+                    <div className="basis-sm">
+                        <ul className="grid grid-cols-1 gap-2 basic-sm">
+                            <li className="basis-sm">CHÍNH SÁCH</li>
+                            <li className="basis-sm" href='#'>
+                                <a href='#' className='hover:underline'>Chính sách bảo hành</a>
+                            </li>
+                            <li className="basis-sm" href='#'>
+                                <a href='#' className='hover:underline'>Chính sách đổi trả</a></li>
+                            <li className="basis-sm" href='#'>
+                                <a href='#' className='hover:underline'>Chính sách bảo mật</a></li>
+                            <li className="basis-sm" href='#'>
+                                <a href='#' className='hover:underline'>Chính sách trả góp</a></li>
+                            <li className="basis-sm" href='#'>
+                                <a href='#' className='hover:underline'>Chính sách khui hộp sản phẩm</a></li>
+                            <li className="basis-sm" href='#'>
+                                <a href='#' className='hover:underline'>Chính sách giao hàng & lắp đặt</a></li>
+                            <li className="basis-sm" href='#'>
+                                <a href='#' className='hover:underline'>Chính sách mạng di động FPT</a></li>
+                            <li className="basis-sm" href='#'>
+                                <a href='#' className='hover:underline'>Chính sách thu thập & xử lí dữ liệu cá nhân</a></li>
+                            <li className="basis-sm" href='#'>
+                                <a href='#' className='hover:underline'>Quy định về hỗ trợ kĩ thuật & sao lưu dữ liệu</a></li>
+                        </ul>
+                    </div>
+                    <div className="basis-sm">
+                        <ul className="grid grid-cols-1 gap-2 basic-sm">
+                            <li className="basis-sm">HỖ TRỢ THANH TOÁN</li>
+                            <li className='basis-sm'>
+                                <ul className="flex">
+                                    <li className="flex flex-nowrap ml-1">
+                                        <a href='#'>
+                                            <img src='https://cdn2.fptshop.com.vn/svg/visa_icon_44fe6e15ed.svg?w=48&q=100%201x,%20https://cdn2.fptshop.com.vn/svg/visa_icon_44fe6e15ed.svg?w=96&q=100%202x' width={48} height={30}></img>
+                                        </a>
+                                    </li>
+                                    <li className="flex ml-1">
+                                        <a href='#'>
+                                            <img src='https://cdn2.fptshop.com.vn/svg/mastercard_icon_c75f94f6a5.svg?w=48&q=100 1x, https://cdn2.fptshop.com.vn/svg/mastercard_icon_c75f94f6a5.svg?w=96&q=100 2x' width={48} height={30}></img>
+
+                                        </a>
+                                    </li>
+                                    <li className="flex ml-1">
+                                        <a href='#'>
+                                            <img src="https://cdn2.fptshop.com.vn/svg/jcb_icon_214783937c.svg?w=48&q=100 1x, https://cdn2.fptshop.com.vn/svg/jcb_icon_214783937c.svg?w=96&q=100 2x" width={48} height={30}></img>
+
+                                        </a>
+                                    </li>
+                                    <li className="flex ml-1">
+                                        <a href='#'>
+                                            <img src="https://cdn2.fptshop.com.vn/svg/amex_icon_d6fb68108d.svg?w=48&q=100 1x, https://cdn2.fptshop.com.vn/svg/amex_icon_d6fb68108d.svg?w=96&q=100 2x" width={48} height={30}></img>
+                                        </a></li>
+                                    
+                                </ul>
+                            </li>
+                            <li className='basis-sm'>
+                                <ul className="flex">
+                                    <li className="flex flex-nowrap ml-1">
+                                        <a href='#'>
+                                            <img src='https://cdn2.fptshop.com.vn/svg/vnpay_icon_f42045057d.svg?w=48&q=100 1x, https://cdn2.fptshop.com.vn/svg/vnpay_icon_f42045057d.svg?w=96&q=100 2x' width={48} height={30}></img>
+                                        </a>
+                                    </li>
+                                    <li className="flex ml-1">
+                                        <a href='#'>
+                                            <img src='https://cdn2.fptshop.com.vn/svg/zalopay_icon_26d64ea93f.svg?w=48&q=100 1x, https://cdn2.fptshop.com.vn/svg/zalopay_icon_26d64ea93f.svg?w=96&q=100 2x' width={48} height={30}></img>
+
+                                        </a>
+                                    </li>
+                                    <li className="flex ml-1">
+                                        <a href='#'>
+                                            <img src='https://cdn2.fptshop.com.vn/svg/napas_icon_94d5330e3c.svg?w=48&q=100 1x, https://cdn2.fptshop.com.vn/svg/napas_icon_94d5330e3c.svg?w=96&q=100 2x' width={48} height={30}></img>
+
+                                        </a>
+                                    </li>
+                                    <li className="flex ml-1">
+                                        <a href='#'>
+                                            <img src='https://cdn2.fptshop.com.vn/svg/kredivo_icon_04f72baf36.svg?w=48&q=100 1x, https://cdn2.fptshop.com.vn/svg/kredivo_icon_04f72baf36.svg?w=96&q=100 2x' width={48} height={30}></img>
+                                        </a></li>
+                                    
+                                </ul>
+                            </li>
+                            <li className='basis-sm'>
+                                <ul className="flex">
+                                    <li className="flex flex-nowrap ml-1">
+                                        <a href='#'>
+                                            <img src='https://cdn2.fptshop.com.vn/svg/momo_icon_baef21b5f7.svg?w=48&q=100 1x, https://cdn2.fptshop.com.vn/svg/momo_icon_baef21b5f7.svg?w=96&q=100 2x' width={48} height={30}></img>
+                                        </a>
+                                    </li>
+                                    <li className="flex ml-1">
+                                        <a href='#'>
+                                            <img src='https://cdn2.fptshop.com.vn/svg/foxpay_icon_063b36c1f8.svg?w=48&q=100 1x, https://cdn2.fptshop.com.vn/svg/foxpay_icon_063b36c1f8.svg?w=96&q=100 2x' width={48} height={30}></img>
+
+                                        </a>
+                                    </li>
+                                    <li className="flex ml-1">
+                                        <a href='#'>
+                                            <img src='https://cdn2.fptshop.com.vn/svg/alepay_icon_20d5310617.svg?w=48&q=100 1x, https://cdn2.fptshop.com.vn/svg/alepay_icon_20d5310617.svg?w=96&q=100 2x' width={48} height={30}></img>
+
+                                        </a>
+                                    </li>
+                                    <li className="flex ml-1">
+                                        <a href='#'>
+                                            <img src='https://cdn2.fptshop.com.vn/svg/muadee_icon_5e297d9e61.svg?w=48&q=100 1x, https://cdn2.fptshop.com.vn/svg/muadee_icon_5e297d9e61.svg?w=96&q=100 2x' width={48} height={30}></img>
+                                        </a></li>
+                                    
+                                </ul>
+                            </li>
+                            <li className='basis-sm'>
+                                <ul className="flex">
+                                    <li className="flex flex-nowrap ml-1">
+                                        <a href='#'>
+                                            <img src='https://cdn2.fptshop.com.vn/svg/homepaylater_icon_adef600842.svg?w=48&q=100 1x, https://cdn2.fptshop.com.vn/svg/homepaylater_icon_adef600842.svg?w=96&q=100 2x' width={48} height={30}></img>
+                                        </a>
+                                    </li>
+                                    <li className="flex ml-1">
+                                        <a href='#'>
+                                            <img src='https://cdn2.fptshop.com.vn/svg/applepay_icon_cb6806a0d0.svg?w=48&q=100 1x, https://cdn2.fptshop.com.vn/svg/applepay_icon_cb6806a0d0.svg?w=96&q=100 2x' width={48} height={30}></img>
+
+                                        </a>
+                                    </li>
+                                    <li className="flex ml-1">
+                                        <a href='#'>
+                                            <img src='https://cdn2.fptshop.com.vn/svg/samsungpay_icon_0292aa9876.svg?w=48&q=100 1x, https://cdn2.fptshop.com.vn/svg/samsungpay_icon_0292aa9876.svg?w=96&q=100 2x' width={48} height={30}></img>
+
+                                        </a>
+                                    </li>
+                                    <li className="flex ml-1">
+                                        <a href='#'>
+                                            <img src='https://cdn2.fptshop.com.vn/svg/googlepay_icon_afa293cc14.svg?w=48&q=100 1x, https://cdn2.fptshop.com.vn/svg/googlepay_icon_afa293cc14.svg?w=96&q=100 2x' width={48} height={30}></img>
+                                        </a></li>
+                                    
+                                </ul>
+                            </li>
+                            
+                            <li className="basis-sm">CHỨNG NHẬN</li>
+                            <li className='basis-sm'>
+                                <ul className="flex">
+                                    <li className="flex flex-nowrap ml-1">
+                                        <a href='#'>
+                                            <img src='https://cdn2.fptshop.com.vn/svg/dmca_icon_8fc6622bd5.svg?w=64&q=100 1x, https://cdn2.fptshop.com.vn/svg/dmca_icon_8fc6622bd5.svg?w=128&q=100 2x' width={52} height={32}></img>
+                                        </a>
+                                    </li>
+                                    <li className="flex ml-1">
+                                        <a href='#'>
+                                            <img src='https://cdn2.fptshop.com.vn/svg/thuong_hieu_manh_2013_icon_b56f772475.svg?w=64&q=100 1x, https://cdn2.fptshop.com.vn/svg/thuong_hieu_manh_2013_icon_b56f772475.svg?w=128&q=100 2x' width={52} height={32}></img>
+
+                                        </a>
+                                    </li>
+                                    <li className="flex ml-1">
+                                        <a href='#'>
+                                            <img src='https://cdn2.fptshop.com.vn/svg/san_pham_dich_vu_hang_dau_viet_nam_icon_282a9ba4f7.svg?w=64&q=100 1x, https://cdn2.fptshop.com.vn/svg/san_pham_dich_vu_hang_dau_viet_nam_icon_282a9ba4f7.svg?w=128&q=100 2x' width={52} height={32}></img>
+
+                                        </a>
+                                    </li>
+                                    <li className="flex ml-1">
+                                        <a href='#'>
+                                            <img src='https://cdn2.fptshop.com.vn/svg/noi_khong_voi_hang_gia_icon_e16037d9cb.svg?w=32&q=100 1x, https://cdn2.fptshop.com.vn/svg/noi_khong_voi_hang_gia_icon_e16037d9cb.svg?w=64&q=100 2x' width={32} height={32}></img>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+                <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+
+                <span className="block text-sm text-gray-500 sm:text-center">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+            </div>
+        </footer>
 
 
-  )
+    )
 }
